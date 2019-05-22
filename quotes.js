bfb350c385e9f58b0d0d177341d2e0fa4a2abd4b
amandaduque <amanda.duque@me.com>
2019-05-22 18:11:24 -0400

    completed

---
+++ quotes.js
@@ -0,0 +1,13 @@
+const quotes = 
+[
+    'You should imagine variables as tentacles, rather than boxes. They do not contain values; they grasp them—two variables can refer to the same value.', 
+    'It is a jackpot when you get to know javascript can be debugged.', 
+    'You’re building your own maze, in a way, and you might just get lost in it.',
+    'JavaScript’s global scope is like a public toilet. You can’t avoid going in there, but try to limit your contact with surfaces when you do.',
+    'Java is to JavaScript as ham is to hamster.'
+
+];
+
+module.exports = {
+    quotes
+}
